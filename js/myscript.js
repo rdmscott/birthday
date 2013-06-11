92ceadfff38c819b235e7405e3d28c8d0f3283b4
Cascadia-rdmscott <rdburmeister@cascadia.edu>
2013-06-10 22:56:43 -0700

    moved location of savefunction

--- js/myscript.js
+++ js/myscript.js
@@ -21,21 +21,7 @@ function onLoad(){
 	document.addEventListener("deviceready", onDeviceReady, false);
 }
 function onDeviceReady() {
-	    console.log("onDeviceReady");
-		if (window.localStorage.length > 1) {
-			for (var i=1; i <  window.localStorage.length; i++) {
-				var keyname = window.localStorage.key(i);
-				var value = window.localStorage.getItem(keyname);
-			
-			// check if birthday needs a reminder
-				$("#view").append("length:" + window.localStorage.length + "Name:" + keyname + "   " + "Value:" + value);
-			}
-		} else {
-			$("p.zero").text("Displaying 0 Birthdays");
-		}
-}
-
-		$("#savebirthday").click(function() {
+	$("#savebirthday").click(function() {
 			//alert("savebirthday");
 			console.log("savebirthday Function");
 		 	var fName = $("#Fname").val();
@@ -53,6 +39,21 @@ function onDeviceReady() {
 			$("#output").html("Save completed");
 			//onDeviceReady();
 		});
+	    console.log("onDeviceReady");
+		if (window.localStorage.length > 1) {
+			for (var i=1; i <  window.localStorage.length; i++) {
+				var keyname = window.localStorage.key(i);
+				var value = window.localStorage.getItem(keyname);
+			
+			// check if birthday needs a reminder
+				$("#view").append("length:" + window.localStorage.length + "Name:" + keyname + "   " + "Value:" + value);
+			}
+		} else {
+			$("p.zero").text("Displaying 0 Birthdays");
+		}
+}
+
+		
 	
 
 
