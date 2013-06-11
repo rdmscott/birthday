8500aa4360188b588b462dac75c275aa9f1d8600
Cascadia-rdmscott <rdburmeister@cascadia.edu>
2013-06-10 21:14:10 -0700

    moved save to inside onDeviceReady + console logs

--- js/myscript.js
+++ js/myscript.js
@@ -1,21 +1,7 @@
 // JavaScript Document
 $(document).ready(function() {
 	console.log("in documentReady");
-	$("#savebirthday").click(function() {
-		 var fName = $("#Fname").val();
-		 var lName = $("#Lname").val();
-		 var month = $("#bmonth").val();
-		 var day   = $("#bday").val();
-		 var year  = $("#byear").val();
-		//alert(fName);
-		var name = lName + ", " + fName;
-		var date = month + "/" + day + "/" + year ;
-		//alert(name+date);
-		console.log(name + date);
-		window.localStorage.setItem(name, date);
-		$("#output").html("Save completed");
-		//onDeviceReady();
-	});
+	
 	
 //  $("#deletebirthday").click(function() {
 //		var name = fname1 + " " + lname2;
@@ -35,18 +21,36 @@ $(document).ready(function() {
 	document.addEventListener("deviceready", onDeviceReady, false);
 	
 	function onDeviceReady() {
-	    console.log("in onDeviceReady routine");
+	    console.log("onDeviceReady");
 		if (window.localStorage.length > 1) {
 		for (var i=1; i <  window.localStorage.length; i++) {
-			var name = window.localStorage.key(i);
-			var value = window.localStorage.getItem(name);
+			var keyname = window.localStorage.key(i);
+			var value = window.localStorage.getItem(keyname);
 			
 			// check if birthday needs a reminder
-			$("#view").append("length:" + window.localStorage.length + "Name:" + name + "   " + "Value:" + value);
+			$("#view").append("length:" + window.localStorage.length + "Name:" + keyname + "   " + "Value:" + value);
 		}
 		} else {
 			$("p.zero").text("Displaying 0 Birthdays");
 		}
+		$("#savebirthday").click(function() {
+			//alert("savebirthday");
+			console.log("savebirthday Function");
+		 	var fName = $("#Fname").val();
+		 	var lName = $("#Lname").val();
+		 	var month = $("#bmonth").val();
+		 	var day   = $("#bday").val();
+		 	var year  = $("#byear").val();
+			//alert(fName);
+			console.log(fName); 
+			var name = lName + ", " + fName;
+			var date = month + "/" + day + "/" + year ;
+			//alert(name+date);
+			console.log(name + date);
+			window.localStorage.setItem(name, date);
+			$("#output").html("Save completed");
+			//onDeviceReady();
+		});
 	}
 
 
