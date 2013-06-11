8a225c2a72f7c49e7f3c5babf32faaac4c415d0d
Cascadia-rdmscott <rdburmeister@cascadia.edu>
2013-06-11 12:48:04 -0700

    trying $(document).delegate

--- js/myscript.js
+++ js/myscript.js
@@ -22,7 +22,7 @@ function onDeviceReady() {
 	//$(document).ready(function() {
 		console.log("documentReady");
 		var clickOk = true;
-		$(document).bind("#savebirthday", "click", function() {
+		$(document).delegate("#savebirthday", "click", function() {
 			//if (clickOk === true) {
         	//	clickOk = false;
         	//	setTimeout(function () {
