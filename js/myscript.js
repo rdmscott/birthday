217b571c110e021964cc1c4a6495a7c1a75861be
Cascadia-rdmscott <rdburmeister@cascadia.edu>
2013-06-12 01:26:10 -0700

    chgd view to date then name

--- js/myscript.js
+++ js/myscript.js
@@ -24,7 +24,7 @@ if (window.localStorage.length > 1) {
 			// check if birthday needs a reminder
 			//	$("#view").append("length:" + window.localStorage.length + "Name:" + keyname + "   " + "Value:" + value);
 				var space = "    ";
-				var displayname = "<li>" + keyname + "&nbsp" + "&nbsp" + "&nbsp" + space + value + "</li>";
+				var displayname = "<li>" + value + "&nbsp" + "&nbsp" + "&nbsp" + space + keyname + "</li>";
 				$("#mylistview").append(displayname);
 			//	$("#view").append(keyname + space + value + "\r\n");
 			    
@@ -64,7 +64,7 @@ function onDeviceReady() {
 			window.localStorage.setItem(name, date);
 			$("#output").html("Save completed");
 			
-			var displayname = "<li>" + name + "&nbsp" + "&nbsp" + "&nbsp" + space + date + "</li>";
+			var displayname = "<li>" + date + "&nbsp" + "&nbsp" + "&nbsp" + space + name + "</li>";
 			$("#mylistview").append(displayname);
 			//$("#mylistview").append(name + space + date);
 			$("#Fname").val('');
